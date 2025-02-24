f6687b3b7cda27d6a1db863f4a1d102bf6a50907
badmazzz <pmeshw12012004@gmail.com>
2025-02-24 11:27:04 +0530

    Initialized frontend setup with React.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Welcome to MEDITRACK</h1>
+      <p>Your medical e-commerce solution</p>
+    </div>
+  );
+};
+
+export default App;
